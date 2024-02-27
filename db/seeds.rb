6903ba62c38b78fde8b714865ba8e275ca3df9f1
AndriusFranz <afortunato39@academic.rrc.ca>
2024-02-26 18:39:19 -0600

    Part 2 Push

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,27 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-676.times do
-    Product.create!(
-        title: Faker::Commerce.product_name,
-        price: Faker::Commerce.price,
-        stock_quantity: Faker::Number.between(from:1, to:100)
-    )
+# 676.times do
+#     Product.create!(
+#         title: Faker::Commerce.product_name,
+#         price: Faker::Commerce.price,
+#         stock_quantity: Faker::Number.between(from:1, to:100)
+#     )
+# end 
+
+require 'csv'
+
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
+
+products = CSV.parse(csv_data, headers: true)
+
+products.each do |product|
+  category = Category.find_or_create_by(name: product['category'])
+  category.products.create(
+    title: product['name'],
+    description: product['description'],
+    price: product['price'],
+    stock_quantity: product['stock quantity']
+  )
 end
